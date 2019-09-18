6c9e47c70a66bd7c422b46ef07448be80e8f3314
BuildTools <arthurpisakhov1@gmail.com>
2019-09-18 17:21:59 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import TabNav from "./components/TabNav.js";
+import Header from "./components/Header.js";
+
+
+export default function App() {
+  return (
+    <main>
+      <Header />
+      {/* <TabNav /> */}
+    </main>
+  );
+}
